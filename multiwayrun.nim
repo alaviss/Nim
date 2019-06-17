cc3960e2fdf551d1325c519f43c9c647cf8ef204
Leorize <leorize+oss@disroot.org>
2019-06-18 01:32:17 +0700

    multiwayrun: more running schemes

--- multiwayrun.nim
+++ multiwayrun.nim
@@ -1,13 +1,18 @@
 import os, osproc, strutils
 
 proc main() =
-  if paramCount() != 1:
+  if paramCount() <= 0:
     quit """Usage: $# <shell command>""" % [getAppFilename()]
 
-  let exe = paramStr 1
+  let
+    exe = paramStr 1
+    subproc = paramCount() > 1
   echo "Running ", exe, " with execShellCmd"
   echo "Error code: ", execShellCmd exe
   echo "Running ", exe, " with execCmdEx"
   echo execCmdEx exe
+  if not subproc:
+    echo "Running as subproc"
+    discard execShellCmd quoteShellCommand [getAppFilename(), paramStr 1, "sub"]
 
 when isMainModule: main()
